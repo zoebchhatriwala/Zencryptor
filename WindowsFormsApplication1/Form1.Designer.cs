45d0185f28501c66858472d903365a068bccb106
unknown <Zoeb Chhatriwala>
2014-10-11 14:45:21 +0530

    first commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.input = new System.Windows.Forms.TextBox();
+            this.key = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.decrypt = new System.Windows.Forms.Button();
+            this.encrypt = new System.Windows.Forms.Button();
+            this.copy = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.about = new System.Windows.Forms.Button();
+            this.Encryption = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.input.Location = new System.Drawing.Point(10, 16);
+            this.input.MaxLength = 2592;
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.input.Size = new System.Drawing.Size(744, 121);
+            this.input.TabIndex = 0;
+            this.input.Text = "Input";
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // key
+            // 
+            this.key.Location = new System.Drawing.Point(111, 143);
+            this.key.MaxLength = 22;
+            this.key.Multiline = true;
+            this.key.Name = "key";
+            this.key.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.key.Size = new System.Drawing.Size(128, 31);
+            this.key.TabIndex = 2;
+            this.key.Text = "0";
+            this.key.TextChanged += new System.EventHandler(this.key_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(12, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Encryption Key:-";
+            // 
+            // decrypt
+            // 
+            this.decrypt.Location = new System.Drawing.Point(389, 143);
+            this.decrypt.Name = "decrypt";
+            this.decrypt.Size = new System.Drawing.Size(138, 31);
+            this.decrypt.TabIndex = 4;
+            this.decrypt.Text = "Decrypt";
+            this.decrypt.UseVisualStyleBackColor = true;
+            this.decrypt.Click += new System.EventHandler(this.decrypt_Click);
+            // 
+            // encrypt
+            // 
+            this.encrypt.Location = new System.Drawing.Point(245, 143);
+            this.encrypt.Name = "encrypt";
+            this.encrypt.Size = new System.Drawing.Size(138, 31);
+            this.encrypt.TabIndex = 5;
+            this.encrypt.Text = "Encrypt";
+            this.encrypt.UseVisualStyleBackColor = true;
+            this.encrypt.Click += new System.EventHandler(this.encrypt_Click);
+            // 
+            // copy
+            // 
+            this.copy.Location = new System.Drawing.Point(533, 143);
+            this.copy.Name = "copy";
+            this.copy.Size = new System.Drawing.Size(138, 31);
+            this.copy.TabIndex = 6;
+            this.copy.Text = "Copy";
+            this.copy.UseVisualStyleBackColor = true;
+            this.copy.Click += new System.EventHandler(this.copy_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(677, 143);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(60, 31);
+            this.clear.TabIndex = 7;
+            this.clear.Text = "Clear X";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // about
+            // 
+            this.about.Location = new System.Drawing.Point(661, 310);
+            this.about.Name = "about";
+            this.about.Size = new System.Drawing.Size(67, 22);
+            this.about.TabIndex = 8;
+            this.about.Text = "About";
+            this.about.UseVisualStyleBackColor = true;
+            this.about.Click += new System.EventHandler(this.about_Click);
+            // 
+            // Encryption
+            // 
+            this.Encryption.Location = new System.Drawing.Point(13, 345);
+            this.Encryption.Name = "Encryption";
+            this.Encryption.Size = new System.Drawing.Size(745, 22);
+            this.Encryption.TabIndex = 9;
+            this.Encryption.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // output
+            // 
+            this.output.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.output.Location = new System.Drawing.Point(10, 180);
+            this.output.MaxLength = 2592;
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.output.Size = new System.Drawing.Size(744, 162);
+            this.output.TabIndex = 10;
+            this.output.Text = "output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(762, 372);
+            this.Controls.Add(this.Encryption);
+            this.Controls.Add(this.about);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.copy);
+            this.Controls.Add(this.encrypt);
+            this.Controls.Add(this.decrypt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.output);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(782, 415);
+            this.Name = "Form1";
+            this.Text = "ZEncryptor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox key;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button decrypt;
+        private System.Windows.Forms.Button encrypt;
+        private System.Windows.Forms.Button copy;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button about;
+        private System.Windows.Forms.Label Encryption;
+        private System.Windows.Forms.TextBox output;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CenterToScreen();
+            
+        }
+        
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+            output.Text = "";
+            Encryption.Text = "";
+            
+        }
+
+        private void key_TextChanged(object sender, EventArgs e)
+        {
+           
+          if (key.Text.Length >= 21)
+            {
+               
+                MessageBox.Show("Key Upto Only 21 Characters","Error");
+                key.Text = key.Text.Substring(0, key.Text.Length - 1);
+                
+            }
+            
+        }
+
+        private void encrypt_Click(object sender, EventArgs e)
+        {
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(input.Text.ToString());
+            int num=0;
+            output.Text = "";
+            if (key.Text == "")
+            {
+                MessageBox.Show("key Field Empty", "Error");
+                key.Text = "0";
+            }
+            
+            foreach (byte b in asciiBytes)
+            {
+                
+                num++;
+              if (b >= 65 && b <= 90 || b >= 97 && b <= 122 || b >= 48 && b <= 57)
+                {
+                   encryptalgo(b,num);
+                }
+               else
+                {
+                    if (((char)b).ToString() == " ")
+                    {
+                        output.Text += ((char)b).ToString();
+                        num--;
+                    }
+                    else if (b == 13 || b == 10)
+                    {
+                        output.Text += ((char)b).ToString();
+                        num = 0;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Symbol: " + ((char)b).ToString() + " Will Not Get Encrypted", "Error");
+                        output.Text += ((char)b).ToString();
+                    }
+                }
+                
+            }
+            
+            
+        }
+
+
+
+      public  void encryptalgo(byte x,int num)
+        {
+        int counter = keygen(num);
+        while (counter!=0)
+                {
+                    x += 1;
+                    counter = counter-1;
+                    if (x>90 && x<97)
+                    {
+                        x = byte.Parse("97");
+                        
+                    }
+                   else if (x > 57 && x < 65)
+                    {
+                        x = byte.Parse("65");
+
+                    }
+                   
+                    else if (x>122)
+                    {
+                        x = byte.Parse("48");
+                    }
+                    }
+             output.Text += ((char) x).ToString();
+             
+            Encryption.Text = "Encrypted";
+        }
+
+      public void decryptalgo(byte x,int num)
+      {
+          int counter = keygen(num) ;
+          while (counter!=0)
+              {
+                  x -= 1;
+                  counter = counter - 1;
+                  if (x < 97 && x>90)
+                  {
+                      x = byte.Parse("90");
+                  }
+                  else if (x > 57 && x < 65)
+                  {
+                      x = byte.Parse("57");
+
+                  }
+                  
+                  else if (x < 48)
+                  {
+
+                      x = byte.Parse("122");
+                  }
+
+              }
+           output.Text += ((char)x).ToString();
+           Encryption.Text = "Decrypted";
+      }
+
+      private void decrypt_Click(object sender, EventArgs e)
+      {
+          byte[] asciiBytes = Encoding.ASCII.GetBytes(input.Text.ToString());
+          int num = 0;
+          output.Text = "";
+          if (key.Text == "")
+          {
+              MessageBox.Show("Key Field Empty", "Error");
+              key.Text = "0";
+          }
+          
+          foreach (byte b in asciiBytes)
+          {
+              num++;
+              if (b >= 65 && b <= 90 || b >= 97 && b <= 122 || b >= 48 && b <= 57)
+              {
+                  decryptalgo(b,num);
+              }
+             else
+              {
+                  if (((char)b).ToString() == " ")
+                  {
+
+                      output.Text += ((char)b).ToString();
+                      num--;
+                  }
+                  else if (b == 13 || b == 10)
+                  {
+                      output.Text += ((char)b).ToString();
+                      num = 0;
+                  }
+                  else
+                  {
+                      MessageBox.Show("Symbol: " + ((char)b).ToString() + " Will Not Get Encrypted", "Error");
+                      output.Text += ((char)b).ToString();
+                  }
+              }
+          }
+          
+
+      }
+
+      private void copy_Click(object sender, EventArgs e)
+      {
+          if (output.Text != "")
+          {
+              Clipboard.SetText(output.Text.ToString());
+          }
+      }
+
+      private void clear_Click(object sender, EventArgs e)
+      {
+          input.Text = "";
+          output.Text = "";
+          key.Text = "";
+
+      }
+
+      private void about_Click(object sender, EventArgs e)
+      {
+          MessageBox.Show("ZEncryptor is a Text Encryptor Application\nWhich Encrypts Confidential Text With A Encryption Key, The Encrypted Text Can be Stored In Any Storage Medium Without Fearing of Theft.\nNote: Donot Reveal or Forget Your Key.\nApplication Developed By: Zoeb Chhatriwala", "About");
+      }
+      private int keygen(int num)
+      {
+          string keyValue = key.Text.ToString();
+          if (num > keyValue.Length)
+          {
+              num = num % keyValue.Length;
+              if (num == 0)
+              {
+                  num = keyValue.Length;
+              }
+          }
+          int  keyone=check(keyValue[num-1]);
+          int keytwo = keyone % keyValue.Length;
+          if (keytwo==0)
+          {
+              keytwo = keyValue.Length;
+          }
+          keytwo = check(keyValue[keytwo-1]);
+          if (keyone <=9 && keytwo <=9)
+          {
+              keyone = keyone * 10;
+              keyone = keyone + keytwo;
+          }
+          else
+          {
+              keyone = keyone + keytwo;
+          }
+          return (keyone);
+      }
+      private int check(int ch)
+      {
+          if (ch >= 48 && ch <= 57)
+          {
+              ch = ch % 48;
+              return ch;
+          }
+          else
+          {
+              return ch;
+          }
+      }
+
+    }
+}
